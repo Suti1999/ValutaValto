6ced36d9bcc954c7e4a3bee960eb0d6f4b345d49
Suti1999 <ozsvartbence11@gmail.com>
2024-03-05 10:53:32 +0100

    Add project files.

---
+++ ValutaValto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Valutak;
+
+namespace ValutaValto
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<Valuta> valutak = await ValutaAdatok();
+            foreach (Valuta valuta in valutak)
+            {
+                Console.WriteLine($"{valuta.Base} - {valuta.Date}");
+            }
+            Console.WriteLine("Program vége!");
+            Console.ReadLine();
+        }
+
+        private static async Task<List<Valuta>> ValutaAdatok()
+        {
+            List<Valuta> valutak = new List<Valuta>();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://infojegyzet.hu/webszerkesztes/php/valuta/api/v1/arfolyam/");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+                valutak.Add(Valuta.FromJson(jsonString));
+            }
+            return valutak;
+        }
+    }
+}
